ebe26e28e3a3d4a58840dd0a0fdeb3582975f8e5
athompson00 <athompson00@stuy.edu>
2019-03-04 20:38:14 -0500

    created and teste solveHelper function

--- Maze.java
+++ Maze.java
@@ -23,25 +23,6 @@ public class Maze{
         System.out.println("need valid filename");
       }
     }
-
-    /*Constructor loads a maze text file, and sets animate to false by default.
-
-      1. The file contains a rectangular ascii maze, made with the following 4 characters:
-      '#' - Walls - locations that cannot be moved onto
-      ' ' - Empty Space - locations that can be moved onto
-      'E' - the location of the goal (exactly 1 per file)
-
-      'S' - the location of the start(exactly 1 per file)
-
-      2. The maze has a border of '#' around the edges. So you don't have to check for out of bounds!
-
-
-      3. When the file is not found OR the file is invalid (not exactly 1 E and 1 S) then:
-
-         throw a FileNotFoundException or IllegalStateException
-
-    */
-
     public Maze(String filename) throws FileNotFoundException{
       try{
         //COMPLETE CONSTRUCTOR
@@ -97,17 +78,6 @@ public class Maze{
         System.out.println("\033[2J\033[1;1H");
 
     }
-
-
-
-
-
-
-   /*Return the string that represents the maze.
-
-     It should look like the text file with some characters replaced.
-
-    */
     public String toString(){
       String result = "";
       for (int i = 0; i < maze.length; i++){
@@ -120,14 +90,6 @@ public class Maze{
 
     }
 
-
-
-    /*Wrapper Solve Function returns the helper function
-
-      Note the helper function has the same name, but different parameters.
-      Since the constructor exits when the file is not found or is missing an E or S, we can assume it exists.
-
-    */
     public int solve(){
       int r = 0;
       int c = 0;
@@ -143,23 +105,6 @@ public class Maze{
       return solve(r, c, 0);
     }
 
-    /*
-      Recursive Solve function:
-
-      A solved maze has a path marked with '@' from S to E.
-
-      Returns the number of @ symbols from S to E when the maze is solved,
-      Returns -1 when the maze has no solution.
-
-
-      Postcondition:
-
-        The S is replaced with '@' but the 'E' is not.
-
-        All visited spots that were not part of the solution are changed to '.'
-
-        All visited spots that are part of the solution are changed to '@'
-    */
     private int solve(int r, int c, int result){ //you can add more parameters since this is private
 
 
